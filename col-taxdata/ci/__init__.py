03245795dd9a9d2545db1d56400f406098cb83f7
balamacab <balamacab@users.noreply.github.com>
2026-09-23 22:39:04 -0500

    ci: reconcile tested issue 28 implementation

--- col-taxdata/ci/__init__.py
+++ col-taxdata/ci/__init__.py
@@ -1,1 +1,3 @@
-"""Repository automation helpers for col-taxdata."""
+"""Repository automation policy for col-taxdata."""
+
+[executed on device: morichalserver (2bfaa62f-23ff-40e5-9fc7-7c1a7d28b1fc)]
